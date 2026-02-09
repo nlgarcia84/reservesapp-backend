ac87d8410b62b9be1ae6fcd5f07a6ffcd15dbd9e
Norman Leyva Garcia <75862898+nlgarcia84@users.noreply.github.com>
2026-02-09 23:37:44 +0100

    he agregado el CORS dentro del controller para que pueda conectarse el frontend desde react

--- src/main/java/com/reservas/reservas/controller/RoomController.java
+++ src/main/java/com/reservas/reservas/controller/RoomController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/rooms")
+@CrossOrigin(origins = "http://localhost:5173")
 public class RoomController {
     private RoomService roomService;
 
